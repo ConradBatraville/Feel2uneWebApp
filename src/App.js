ad1d121bc5d9e07a6c37e97022533d95869bb437
Conrad <theradistone@gmail.com>
2019-05-29 21:33:59 -0400

    add dummy data to App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+let fakeServerData = {
+  user: {
+    name: 'Chanelle',
+    track: 
+      {
+        name: 'Dedication (feat. Kendrick Lamar)',
+        artist: 'Nipsey Hussle',
+        album: 'Victory Lap'
+      }
+  }
+}
 
 class CurrentlyPlaying extends Component {
   render() {
+    console.log(this.props)
     return(
       <div style={{width:'60%'}}>
         <img/>
-        <h3 style={{color: 'blue'}}>Song Title</h3>
-        <h4 style={{color: 'red'}}>Artist Name</h4>
-        <h5 style={{color: 'red'}}>Album Name</h5>
+        <h3 style={{color: 'blue'}}>{this.props.track && this.props.track.name}</h3>
+        <h4 style={{color: 'red'}}>{this.props.track && this.props.track.artist}</h4>
+        <h5 style={{color: 'red'}}>{this.props.track && this.props.track.album}</h5>
       </div>
     )
   }
@@ -26,14 +39,28 @@ class Mood extends Component {
 }
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {serverData: {}}
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({serverData: fakeServerData}), 1000)
+  }
   render() {
     return (
       <div className="App">
-        <h2 >Welcome to Feel2une</h2>
-        <CurrentlyPlaying/>
-        <Mood/>
-        <Mood/>
-        <Mood/>
+        { this.state.serverData.user ?
+        <div>
+          <h1 style={{'font-size': '50px'}}>
+            Hey{this.state.serverData.user.name}, let's Feel2une !
+          </h1>
+          <CurrentlyPlaying track={this.state.serverData.user.track} />
+          <Mood/>
+          <Mood/>
+          <Mood/>
+        </div> : <h1>Loading ....</h1>
+        }
       </div>
     );
   }
